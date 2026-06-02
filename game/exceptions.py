5b7b754577bbd2cb25bc31e0a63ea6c28a354d30
Anastasiia <anastasiia.hainova@gmail.com>
2026-06-02 10:12:19 +0200

    Base structure of project created. Initial data added to Exceptions, Settings.

---
+++ game/exceptions.py
@@ -0,0 +1,5 @@
+class InvalidInputError(Exception):
+    pass
+
+class InvalidRollError(Exception):
+    pass
---
+++ game/settings.py
@@ -0,0 +1,11 @@
+GAME_LEVELS = {
+      1: 5,
+      2: 8,
+      3: 10
+  }
+
+GAME_LEVELS_CONVERT = {
+       5 : "Short",
+       8 : "Medium",
+       10 : "Long" 
+  }
